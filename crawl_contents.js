7693b694252e11eb9efbd69ae09dd392d19cb08b
Minseok Jeon <cheon7886@naver.com>
2019-02-09 19:22:36 +0900

    numbering fixed

--- crawl_contents.js
+++ crawl_contents.js
@@ -4,20 +4,35 @@ var dst = casper.cli.options["dst"];
 var toon_id = casper.cli.options["id"];
 var lastno = casper.cli.options["no"];
 var no;
+
 casper.start();
+
 for (no=1; no<=lastno;no++){
     var nox = no;
     var uri ='http://comic.naver.com/webtoon/detail.nhn?titleId=' + toon_id + '&no=' + nox;
+
     (function(m){
         casper.thenOpen(uri, function() {
             var i;
+
             for(i=0;;i++){
                 var id = '#content_image_' + i;
                 if( casper.exists(id) == false ) break;
-                this.captureSelector(m + '_' + i + '.png', id);
-        }
+				
+				var str1 = "" + m
+				var pad1 = "0000"
+				var ans1 = pad1.substring(0, pad1.length - str1.length) + str1
+				
+				var str2 = "" + i
+				var pad2 = "0000"
+				var ans2 = pad2.substring(0, pad2.length - str2.length) + str2
+                
+				this.captureSelector(ans1 + '_' + ans2 + '.png', id);
+        	}
+
+			console.log('No '+ m + '. has rendered' );
     });
-    console.log('No '+ m + '. has rendered' );
+
 })(nox);
 }
 
